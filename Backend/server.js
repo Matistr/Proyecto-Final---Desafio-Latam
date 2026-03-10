ffa467cdbe9aedbbee8f4d88ccd21d5a584cbb43
Roberto Calzadillo <Robertocalzadillo@gmail.com>
2026-03-10 00:44:52 -0400

    Conectando el fronted de vite con el backend para que se muestre en el servidor que por ahora es 2000

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+
+require('dotenv').config()
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const path = require("path"); 
+
+
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+const puerto = 2000
+
+app.use(express.static(path.join(__dirname,  "..", "Frontend", "dist")))
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'..', 'Frontend', 'index.html'))
+})
+
+app.listen(puerto, ()=>{
+    
+        console.log(`Conectados en el servidor en ${puerto}`)
+        
+ 
+})
