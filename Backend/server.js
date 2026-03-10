237752941e9a320827d0c07c8f9f4296a6799a7c
Mati Sepu <matias.a.sepulveda@gmail.com>
2026-03-09 23:46:16 -0300

    update

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const frontendPath = path.join(__dirname, '../Frontend/dist');
+
+app.use(express.static(frontendPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
